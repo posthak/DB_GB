7bf02d677bb9b6fc282142508f396e29c6e9b3b3
posthak <adler_m@mail.ru>
2023-06-30 11:04:02 +0200

    Init commit task 4

---
+++ задание4.sql
@@ -0,0 +1,85 @@
+CREATE TABLE  AUTO 
+(       
+	REGNUM VARCHAR(10) PRIMARY KEY, 
+	MARK VARCHAR(10), 
+	COLOR VARCHAR(15),
+	RELEASEDT DATE, 
+	PHONENUM VARCHAR(15)
+);
+
+ -- AUTO
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111114,'LADA', 'КРАСНЫЙ', date'2008-01-01', '9152222221');
+
+
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111115,'VOLVO', 'КРАСНЫЙ', date'2013-01-01', '9173333334');
+
+
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111116,'BMW', 'СИНИЙ', date'2015-01-01', '9173333334');
+
+
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111121,'AUDI', 'СИНИЙ', date'2009-01-01', '9173333332');
+
+
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111122,'AUDI', 'СИНИЙ', date'2011-01-01', '9213333336');
+
+
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111113,'BMW', 'ЗЕЛЕНЫЙ', date'2007-01-01', '9214444444');
+
+
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111126,'LADA', 'ЗЕЛЕНЫЙ', date'2005-01-01', null);
+
+
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111117,'BMW', 'СИНИЙ', date'2005-01-01', null);
+
+
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111119,'LADA', 'СИНИЙ', date'2017-01-01', 9213333331);
+
+create table test_a (id INT, test varchar(10));
+insert into test_a(id, test) values
+(10, 'A'),
+(20, 'A'),
+(30, 'F'),
+(40, 'D'),
+(50, 'C');
+create table test_b (id INT);
+insert into test_b(id) values
+(10),
+(30),
+(50);
+
+
+-- Задание 1 
+SELECT color,
+SUM(CASE 
+	WHEN mark = 'BMW' THEN 1
+    ELSE 0
+    END) as BMW,
+SUM(CASE 
+	WHEN mark = 'LADA' THEN 1
+    ELSE 0
+    END) as LADA
+FROM auto
+WHERE mark in ('BMW','LADA')
+GROUP BY color;
+
+-- Задание 2 
+SELECT mark,
+COUNT(*) AS count_this_mark,
+(SELECT COUNT(*) FROM auto)-COUNT(*) AS count_other_mark
+FROM auto
+GROUP BY mark;
+
+-- Задание 3
+SELECT a.*, b.*
+FROM test_a a
+LEFT JOIN test_b b ON a.id = b.id
+WHERE b.id is null;
